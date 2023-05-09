0c74bc519d324ca7bc2ec5fe301184c8fba5b0a7
felixyh <felixyanghui@gmail.com>
2023-05-09 19:37:24 +0800

    update - chatgpt35

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 Run The Application
 To run the application, execute the following command in your terminal:
 
+Linux: 
 ```
 export OPENAI_API_KEY="your_openai_api_key_here"
+python app.py
+```
+
+Windows:
+```
+# cmd: set the variable
+set OPENAI_API_KEY="your_openai_api_key_here"
+# cmd: get the variable
+echo %OPENAI_API_KEY%
+
+# powershell: set the variable
+$env:OPENAI_API_KEY="your_openai_api_key_here"
+# powershell: get the variable
+$env:OPENAI_API_KEY
+
+
 python app.py
 ```
--- app.py
+++ app.py
@@ -5,24 +5,27 @@ from openai.error import RateLimitError
 import os
 
 app = Flask(__name__)
-openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/gtp4', methods=['GET', 'POST'])
-def gpt4():
+@app.route('/gpt35', methods=['GET', 'POST'])
+def gpt35():
     if request.method == 'GET':
         user_input = request.args.get('user_input')
     else:
-        messages = [{"role": "user", "content": user_input}]
-    
+        user_input = request.form['user_input']
+
+    # user_input = request.args.get('user_input') if request.method == 'GET' else request.form['user_input']
+    messages = [{"role": "user", "content": user_input}]
     try:
         response = openai.ChatCompletion.create(
-            model='gpt-4',
+            model='gpt-3.5-turbo',
             messages=messages
         )
+        content = response.choices[0].message["content"]
     except RateLimitError:
         content = "The server is experiencing a high volume of requests. Please try again later."
 
